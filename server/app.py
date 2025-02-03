6e037d785eefd084efb748a9da3c879aa652c8c3
sharonodongo <sharon94odongo@gmail.com>
2025-02-04 00:29:27 +0300

    submitting assignment

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, request, make_response, jsonify
+from flask import Flask, request, make_response, jsonify,abort
 from flask_cors import CORS
 from flask_migrate import Migrate
+from datetime import datetime
 
 from models import db, Message
 
@@ -14,13 +15,41 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
-def messages():
-    return ''
+@app.route('/messages', methods=['GET'])
+def get_messages():
+    messages = Message.query.order_by(Message.created_at).all()
+    return [message.to_dict() for message in messages]
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
+@app.route('/messages', methods = ['POST'])
+def create_message():
+    data = request.get_json()
+    if not data or 'body' not in data or 'username' not in data:
+        abort(404, 'Invalid Input')
+    new_message = Message(body=data['body'], username= data['username'])
+    db.session.add(new_message)
+    db.session.commit()
+
+    return new_message.to_dict(), 201
+
+@app.route('/messages/<int:id>', methods=['PATCH'])
+def update_messages(id):
+    data = request.get_json()
+    message = db.session.get(Message,id)
+    if not message:
+        return jsonify({"Error": "Message not found"})
+    if "body" in data:
+        message.body = data['body']
+        message.updated_at = datetime.utcnow()
+    db.session.commit()
+    return message.to_dict(),200
+
+@app.route('/messages/<int:id>', methods = ['DELETE'])
+def delete_messages_by_id(id):
+    message = db.session.get(Message,id)
+    db.session.delete(message)
+    db.session.commit()
+    return jsonify({'message':'message successfully deleted'}),200
+
 
 if __name__ == '__main__':
     app.run(port=5555)
